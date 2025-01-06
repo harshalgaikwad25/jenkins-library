781338fede6932ae08ca6163e03607b50b469fd2
harshalgaikwad25 <57884624+harshalgaikwad25@users.noreply.github.com>
2025-01-06 20:08:18 +0530

    Create dockerBuild.groovy

---
+++ vars/dockerBuild.groovy
@@ -0,0 +1,15 @@
+def call(String imageName = 'boardgame', String tag = 'latest', String registry = 'harshalgaikwad') {
+    // Ensure Docker is available in the pipeline
+    if (!tool('docker')) {
+        error "Docker tool is not available!"
+    }
+
+    // Perform the Docker build and tag operations
+    withDockerRegistry(credentialsId: 'docker-hub', toolName: 'docker') {
+        // Building the Docker image
+        sh "docker build -t ${imageName} ."
+
+        // Tagging the Docker image
+        sh "docker tag ${imageName} ${registry}/${imageName}:${tag}"
+    }
+}
